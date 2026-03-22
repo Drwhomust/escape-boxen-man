12a5d771b1215ccd073b1051cd4b1841c8ca93af
Drwhomust <rabbids8008@gmail.com>
2026-03-22 14:11:34 -0700

    Update main.lua
    taking a small break. but a lot of progress made!! :D

--- code/main.lua
+++ code/main.lua
@@ -12,18 +12,26 @@ function love.load()
     print("Now loading.... please wait owo :3")
     love.graphics.setDefaultFilter("nearest", "nearest") -- shows the GOOD pixel art
     world = wf.newWorld(0,0, true)
+    world:setGravity(0, 512)
+    world:addCollisionClass('Player')
+    world:addCollisionClass('Walls')
+    world:addCollisionClass('Bad guys')
 
     -- player
     player = {}
     player.x = 200 -- x and y for their starting
     player.y = 200
-    player.speed = 25 -- their speed
+    player.speed = 15 -- their speed
     player.speedyness = 0
     player.mods = {}
-    player.hitbox = world:newRectangleCollider(player.x,player.y,60,70) -- makes their hit box
+    player.hitbox = world:newRectangleCollider(player.x,player.y,70,70) -- makes their hit box
     player.hitbox:setFixedRotation(true) -- to prevent them from getting stuck
+    player.hitbox:setCollisionClass('Player')
     player.emo = "" -- for the emotations
     player.canjump = true
+    player.is_Moving = false
+
+    cam = camera.new()
 
     time_to_win = 232 -- in secounds
 
@@ -31,10 +39,13 @@ function love.load()
     function make_starting() -- makes the starting area
         top_wall = world:newRectangleCollider(200,0,600,100)
         top_wall:setType('static')
+        top_wall:setCollisionClass('Walls')
         left_wall = world:newRectangleCollider(0,0,200,600)
         left_wall:setType('static')
+        left_wall:setCollisionClass('Walls')
         ground = world:newRectangleCollider(200,530,600,70)
         ground:setType('static')
+        ground:setCollisionClass('Walls')
     end
 
     -- fire stuff
@@ -55,20 +66,20 @@ function love.load()
 
         if where == "Right" then -- checks where
 
-            player.x = player.x + speed -- movess
+            player.x = player.x + player.speed -- movess
 
             --pizza tower like. read it and you will get it
-            if speedyness == 0 then
+            if player.speedyness == 0 then
                 timer.after(3, function()
-                    speedyness = 1
-                    speed = speed + 20
+                    player.speedyness = 1
+                    player.speed = player.speed + 20
                 end)
             end
 
-            if speedyness == 1 then
+            if player.speedyness == 1 then
                 timer.after(3, function()
-                    speedyness = 2
-                    speed = speed + 25
+                    player.speedyness = 2
+                    player.speed = player.speed + 25
                 end)
             end
         end
@@ -76,19 +87,19 @@ function love.load()
         ---------------------------------------------
         if where == "Left" then
 
-            player.x = player.x - speed
+            player.x = player.x - player.speed
 
-            if speedyness == 0 then
+            if player.speedyness == 0 then
                 timer.after(3, function()
-                    speedyness = 1
-                    speed = speed + 20
+                    player.speedyness = 1
+                    player.speed = player.speed + 20
                 end)
             end
 
-            if speedyness == 1 then
+            if player.speedyness == 1 then
                 timer.after(3, function()
-                    speedyness = 2
-                    speed = speed + 25
+                    player.speedyness = 2
+                    player.speed = player.speed + 25
                 end)
             end
         end
@@ -129,6 +140,47 @@ function love.load()
 end
 
 function love.update(dt)
+    if player.is_Moving == false then
+        player.speed = 15
+        player.speedyness = 0
+    end
+
+    -- key board con
+    if love.keyboard.isDown("right") then
+        fire.emit("Player Move", "Right")
+        player.is_Moving = true
+    else
+        player.is_Moving = false
+    end
+
+    if love.keyboard.isDown("left") then
+        fire.emit("Player Move", "Left")
+        player.is_Moving = true
+    else
+        player.is_Moving = false
+    end
+
+    if love.keyboard.isDown("up") then
+        fire.emit("Player Move", "Up")
+        player.is_Moving = true
+    else
+        player.is_Moving = false
+    end
+
+    if love.keyboard.isDown("escape") then
+        love.event.quit()
+    end
+
+    player.hitbox:destroy()
+    player.hitbox = world:newRectangleCollider(player.x,player.y,70,70)
+    player.hitbox:setCollisionClass('Player')
+
+    if player.hitbox:enter('Walls') then
+        print("hiT1")
+        player.x = player.x - 30
+        player.y = player.y - 30
+    end
+
     world:update(dt)
     timer.update(dt)
 end
@@ -136,5 +188,11 @@ end
 function love.draw()
     if debugmode == true then -- drawing drawing drawing
         world:draw() -- draws hitboxes
+        love.graphics.print("player x: " .. player.x,10,10)
+        love.graphics.print("player y: " .. player.y,10,25)
+        love.graphics.print("speed " .. player.speed,10,40)
+        love.graphics.print("speedy " .. player.speedyness,10,60)
     end
+
+    camera:lookAt(player.x, player.y)
 end
